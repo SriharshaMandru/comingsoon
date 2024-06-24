b6cc7bcd71ca95dc733b52f58e8f2f55678cc591
SriharshaMandru <mandrusriharsha@gmail.com>
2024-06-24 13:32:15 +0530

    comingsoon files are added

---
+++ coming.js
@@ -0,0 +1,29 @@
+var countDownDate = new Date("Aug 6, 2024 00:00:00").getTime();
+
+    var X = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerHTML = formatTime(days);
+        document.getElementById("hours").innerHTML = formatTime(hours);
+        document.getElementById("minutes").innerHTML = formatTime(minutes);
+        document.getElementById("seconds").innerHTML = formatTime(seconds);
+
+        if (distance < 0) {
+            clearInterval(X);
+            document.getElementById("days").innerHTML = "00";
+            document.getElementById("hours").innerHTML = "00";
+            document.getElementById("minutes").innerHTML = "00";
+            document.getElementById("seconds").innerHTML = "00";
+        }
+    }, 1000);
+
+    function formatTime(time) {
+        return (time < 10 ? "0" : "") + time;
+    }
+
